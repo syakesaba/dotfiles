b9c914c5ccf1f0dc14b59747b0ec3ed741c6a6f2
syakesaba <syakesaba@gmail.com>
2015-08-14 09:16:12 +0900

    pythonstartup script -> adapted BSD

--- .pythonrc.py
+++ .pythonrc.py
@@ -3,24 +3,41 @@
 
 import os,sys,atexit
 
+#===============Completention
 try:
     import rlcompleter
-    #rlcompleter.readline.parse_and_bind("tab: complete")
-    rlcompleter.readline.parse_and_bind("bind ^I rl_complete")
-    #readline.parse_and_bind("bind tab: complete")
-    #readline.parse_and_bind("bind tab: rl_complete")
-    #readline.parse_and_bind("bind ^I complete")
-except Exception as e:
+    import readline
+    if "libedit" in readline.__doc__:
+        #BSD with libedit
+        readline.parse_and_bind("bind -e")
+        readline.parse_and_bind("bind '\t' rl_complete")
+    else:
+        readline.parse_and_bind("tab: complete")
+except ImportError as e:
     pass
+except Exception as e:
+    print "Unknown error at ",__file__,e
+
+#===============History
 try:
     import rlcompleter
+    import readline
     home = os.path.expanduser('~')
     pyhistfile = os.path.join(home, '.pyhistory')
-    if not os.path.exists(pyhistfile):
-        open(pyhistfile,"a").close()
-    rlcompleter.readline.read_history_file(pyhistfile)
-    rlcompleter.readline.set_history_length(100)
-    atexit.register(rlcompleter.readline.write_history_file, pyhistfile)
+    with open(pyhistfile, 'a'):
+        os.utime(pyhistfile, None)
+    if hasattr(readline, "read_history_file"):
+        try:
+            readline.read_history_file(pyhistfile)
+        except IOError as e:
+            #First time
+            pass
+    readline.set_history_length(100)
+    atexit.register(readline.write_history_file, pyhistfile)
 except Exception as e:
-    pass
-sys.ps1 = '\001\033[96m\002>>> \001\033[0m\002'
+    print "Unknown error at ",__file__,e
+
+#===============Miscellaneous
+sys.ps1 = '>> '
+#sys.ps1 = '\001\033[96m\002>>> \001\033[0m\002'
+
